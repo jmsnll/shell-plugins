f9a2910a28267d6ad70836eff544f409ef45b9dc
James Neill <git@jamesneill.codes>
2025-01-15 21:34:04 +0100

    fix(plugin/age): add back operation string representation function

--- plugins/age/operation.go
+++ plugins/age/operation.go
@@ -7,12 +7,23 @@ const (
 	encryptLong  = "--encrypt"
 )
 
-type Operation int
-
 const (
 	Encrypt Operation = iota
 	Decrypt
 )
+
+type Operation int
+
+func (op Operation) String() string {
+	switch op {
+	case Encrypt:
+		return "encrypt"
+	case Decrypt:
+		return "decrypt"
+	default:
+		return "unknown"
+	}
+}
 
 func detectOperation(args []string) Operation {
 	for _, arg := range args {
