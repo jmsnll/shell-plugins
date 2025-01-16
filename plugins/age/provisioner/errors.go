da5471440f33bbb43f53018f5749cadaa236b374
James Neill <contact@jamesneill.co.uk>
2025-01-16 12:46:10 +0100

    refactor(age): move errors into provisioner module

--- plugins/age/provisioner/errors.go
+++ plugins/age/provisioner/errors.go
@@ -1,8 +1,7 @@
-package age
+package provisioner
 
 import "fmt"
 
 var (
 	ErrConflictingIdentityFlag = fmt.Errorf("conflict: the -i/--identity flag is automatically added by this plugin. Remove it from your command to continue")
-	ErrUnknownCommand          = fmt.Errorf("unknown command")
 )
